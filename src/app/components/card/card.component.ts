15495a88f94b6a018f64e3fe693e747500596d19
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-10-05 20:51:32 -0300

    desacoplando seu componente

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  name: string = 'CHARMANDER';
+  attributesTypes: string[] = ['FIRE', 'ROCK'];
 
+  constructor() {}
+  ngOnInit(): void {}
 }
